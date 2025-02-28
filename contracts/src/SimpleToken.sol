fb0231251ebaef59ba7e8d65b51a9bd94662b2b7
alinciprian <ciprian_enita@yahoo.com>
2025-02-28 09:20:23 +0200

    created SimpleToken

---
+++ contracts/src/SimpleToken.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract SimpleToken is ERC20 {
+    uint256 initialSupply;
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call the function");
+        _;
+    }
+
+    constructor(uint256 _initialSupply) ERC20("SimpleToken", "STK") {
+        owner = msg.sender;
+        initialSupply = _initialSupply;
+        _mint(msg.sender, _initialSupply);
+        approve(msg.sender, initialSupply);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyOwner {
+        _burn(_from, _amount);
+    }
+}

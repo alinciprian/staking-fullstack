b04f4ef1b404c41b825f6a5d3ba5a70a3987b407
alinciprian <ciprian_enita@yahoo.com>
2025-03-03 15:10:09 +0200

    functional contract

--- contracts/src/SimpleStaking.sol
+++ contracts/src/SimpleStaking.sol
@@ -9,7 +9,7 @@ contract SimpleStaking is ReentrancyGuard {
     IERC20 public immutable stakingToken;
     IERC20 public immutable rewardToken;
 
-    uint256 immutable PRECISION = 10 ** 18;
+    uint256 immutable PRECISION = 1e18;
 
     address owner;
     uint256 public rewardRate;
@@ -17,21 +17,19 @@ contract SimpleStaking is ReentrancyGuard {
     uint256 totalSupply;
     //last time rewards were distributed
     uint256 public lastRewardTimestamp;
-    // starting Timestamp
-    uint256 public startTimestamp;
     // (reward rate * dt * 1e18)/ totalSupply
     uint256 rewardPerTokenStored;
     // how much did each user stake
     mapping(address => uint256) public balanceOf;
     // rewards to be claimed by each user
     mapping(address => uint256) public rewards;
-    mapping(address => uint256) rewardDebt;
+    //used to make sure each user can only withdraw from the point the stake is made
+    mapping(address => uint256) rewardPerTokenDebt;
 
     constructor(address _stakingToken, address _rewardToken) {
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken);
         rewardToken = IERC20(_rewardToken);
-        startTimestamp = block.timestamp;
     }
 
     modifier onlyOwner() {
@@ -39,34 +37,53 @@ contract SimpleStaking is ReentrancyGuard {
         _;
     }
 
-    modifier updateRewardPerToken() {
-        rewardPerTokenStored += ((block.timestamp - lastRewardTimestamp) * rewardRate * PRECISION) / totalSupply;
+    modifier updateRewardPerToken(address _account) {
+        rewardPerTokenStored = _rewardPerToken();
         lastRewardTimestamp = block.timestamp;
+
+        if (_account != address(0)) {
+            rewards[_account] = _earned(_account);
+            rewardPerTokenDebt[_account] = rewardPerTokenStored;
+        }
         _;
     }
 
-    //There will be a fixed amount of reward that will be distributed over time across all participants
-    //Each participant will get reward proportional to the amount of staked tokens over time
-
-    //Each time deposit() or stake() functions are used by any user, the rewards accumulated will be distributed in the database
-
-    // (rewardPerSecond * (block.timstamp - lastUpdateAt))/ totalSupply - how much a single token has accumulated in an interval of time
-
-    //o functie de calcul pt rewards
-
-    function stake(uint256 _amount) external updateRewardPerToken {
+    function stake(uint256 _amount) external updateRewardPerToken(msg.sender) {
         require(_amount > 0, "amount must be greater than 0");
-
-        rewardDebt[msg.sender] = _amount * rewardPerTokenStored;
         balanceOf[msg.sender] = _amount;
         totalSupply += _amount;
         stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
-    function withdraw(uint256 _amount) external updateRewardPerToken {
+    function withdraw(uint256 _amount) external updateRewardPerToken(msg.sender) {
         require(_amount > 0, "amount must be greater than 0");
         require(_amount <= balanceOf[msg.sender], "not enough balance");
         balanceOf[msg.sender] -= _amount;
         stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function _rewardPerToken() internal view returns (uint256) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+
+        return rewardPerTokenStored + (rewardRate * (block.timestamp - lastRewardTimestamp) * PRECISION) / totalSupply;
+    }
+
+    function _earned(address _account) internal view returns (uint256) {
+        return
+            ((balanceOf[_account] * (_rewardPerToken() - rewardPerTokenDebt[_account])) / PRECISION) + rewards[_account];
+    }
+
+    function getReward() external updateRewardPerToken(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardToken.transfer(msg.sender, reward);
+        }
+    }
+
+    function updateRewardRate(uint256 _newRewardRate) external onlyOwner {
+        rewardRate = _newRewardRate;
     }
 }
